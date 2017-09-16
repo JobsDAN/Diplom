f976eb5d59b8867f7b8c05f1b29649eb8efcb65a
Tokarev Maxim <nepfion96@gmail.com>
2017-09-16 13:34:34 +0300

    Editor mode grid

---
+++ Assets/Scripts/Grid/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell {
+    public bool IsOccupied { get; private set; }
+    public bool IsSelected { get; set; }
+    public Vector3 Position { get; private set; }
+	
+    public Cell(Vector3 position, bool isOccupied = false)
+    {
+        Position = position;
+        IsOccupied = isOccupied;
+        IsSelected = false;
+    }
+}
