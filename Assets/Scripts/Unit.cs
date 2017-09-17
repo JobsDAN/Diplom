46a98703abb6892ddc77642b4f1c9dbfbbc6d1b9
Diadlo <polsha3@gmail.com>
2017-09-17 17:47:47 +0300

    Add unit outlint hightlight on selection

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,16 +7,20 @@ public class Unit : MonoBehaviour {
 	static GameObject selectedUnit;
 	private Cell currentCell;
 
-	// Use this for initialization
-	void Start()
-	{
-		
-	}
-	
 	const int LEFT_MOUSE_BUTTON = 0;
 	const int RIGHT_MOUSE_BUTTON = 1;
 
 	const float SELECTION_UP = 0.1f;
+	Material material;
+	Shader outlineShader;
+	Shader defaultShader;
+
+	void Start()
+	{
+		material = GetComponent<Renderer>().material;
+		defaultShader = material.shader;
+		outlineShader = Shader.Find("Outlined/Silhouetted Bumped Diffuse");
+	}
 
 	// Update is called once per frame
 	void Update()
@@ -75,19 +79,14 @@ public class Unit : MonoBehaviour {
 
 	private void Select()
 	{
-		Vector3 p = transform.position;
-		p.y += SELECTION_UP;
-		transform.position = p;
-
+		material.shader = outlineShader;
+		material.SetColor("_OutlineColor", Color.red);
 		selectedUnit = transform.gameObject;
 	}
 
 	private void Unselect()
 	{
-		Vector3 p = transform.position;
-		p.y -= SELECTION_UP;
-		transform.position = p;
-
+		material.shader = defaultShader;
 		selectedUnit = null;
 	}
 }
