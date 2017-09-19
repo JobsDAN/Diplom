aded1359f5ee3d9029179d318d038103a6773a36
Diadlo <polsha3@gmail.com>
2017-09-19 11:29:19 +0300

    Add Flock smooth moving

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -6,6 +6,9 @@ public class Flock : MonoBehaviour {
 
 	static private Flock selectedFlock;
 
+	private float speed = 0.05f;
+	private Queue<Vector3> path;
+
 	const int LEFT_MOUSE_BUTTON = 0;
 	const int RIGHT_MOUSE_BUTTON = 1;
 
@@ -16,15 +19,35 @@ public class Flock : MonoBehaviour {
 		GameObject groundGameObject = GameObject.Find("Grid");
 		groundCollider = groundGameObject.GetComponent<Collider>();
 		grid = groundGameObject.GetComponent<Grid>();
+		path = new Queue<Vector3>();
 	}
 
 	void Update () {
 		if (Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON)) {
 			SelectClick();
-		} else if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON)) {
-			MoveClick();
+			return;
 		}
 
+		if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON)) {
+			MoveClick();
+			return;
+		}
+
+		if (path.Count == 0) {
+			return;
+		}
+
+		Vector3 dest = path.Peek();
+		Vector3 direction = dest - transform.position;
+		Vector3 step = direction.normalized * speed;
+		float time = Time.deltaTime;
+		float distThisFrame = speed * time;
+		if (direction.magnitude < step.magnitude) {
+			path.Dequeue();
+			transform.position = dest;
+		}
+
+		transform.Translate(step);
 	}
 
 	private void MoveClick() {
@@ -45,7 +68,7 @@ public class Flock : MonoBehaviour {
 		Cell c = grid.CellFromWorldPosition(hit.point);
 		Vector3 newPosition = c.Position;
 		newPosition.y = transform.position.y;
-		transform.position = newPosition;
+		path.Enqueue(newPosition);
 	}
 
 	private void SelectClick() {
