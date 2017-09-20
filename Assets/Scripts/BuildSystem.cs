545fa9eb960e1f71e38aa9fabd3d086ae3bc5b19
Diadlo <polsha3@gmail.com>
2017-09-20 23:03:04 +0300

    Add simple build system

---
+++ Assets/Scripts/BuildSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildSystem : MonoBehaviour {
+
+	[SerializeField]
+	GameObject boxPrefab;
+
+	GameObject currentObject;
+
+	Collider groundCollider;
+
+	void Start() {
+		GameObject groundGameObject = GameObject.Find("Ground");
+		groundCollider = groundGameObject.GetComponent<Collider>();
+	}
+	
+	void Update () {
+		// Follow mouse
+		if (currentObject) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (!groundCollider.Raycast(ray, out hit, Mathf.Infinity)) {
+				return;
+			}
+
+			currentObject.transform.position = hit.point;
+		}
+
+		// Place on left mouse click
+		if (Input.GetMouseButtonDown(0)) {
+			currentObject = null;
+		}
+	}
+
+	public void BuildBox() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (!groundCollider.Raycast(ray, out hit, Mathf.Infinity)) {
+			return;
+		}
+
+		if (currentObject != null) {
+			Destroy(currentObject);
+			currentObject = null;
+			return;
+		}
+
+		Quaternion q = new Quaternion();
+		Vector3 size = boxPrefab.GetComponent<Renderer>().bounds.size;
+		Vector3 pos = hit.point;
+		pos += new Vector3(0, size.y / 2, 0);
+		currentObject = Instantiate<GameObject>(boxPrefab, pos, q);
+	}
+}
