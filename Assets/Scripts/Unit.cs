103f273a0f60961a889d0f26c9f1d7611ea16a74
Diadlo <polsha3@gmail.com>
2017-09-20 10:38:10 +0300

    Add using NavMesh to pathfinding

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,71 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour {
-
-	static GameObject selectedUnit;
-	Cell currentCell;
-
-	const int LEFT_MOUSE_BUTTON = 0;
-	const int RIGHT_MOUSE_BUTTON = 1;
-
-	const float SELECTION_UP = 0.1f;
 	Material material;
 	Shader outlineShader;
 	Shader defaultShader;
 
-	Queue<Vector3> path;
-	float speed = 0.05f;
-	void Awake()
-	{
-		path = new Queue<Vector3>();
-	}
+	NavMeshAgent agent;
 
-	void Start()
-	{
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
 		material = GetComponent<Renderer>().material;
 		defaultShader = material.shader;
 		outlineShader = Shader.Find("Outlined/Silhouetted Bumped Diffuse");
 	}
 
-	void Update()
-	{
-		if (path.Count == 0) {
-			return;
-		}
-
-		Vector3 dest = path.Peek();
-		Vector3 direction = dest - transform.position;
-		Vector3 step = direction.normalized * speed;
-		float time = Time.deltaTime;
-		float distThisFrame = speed * time;
-		if (direction.magnitude < step.magnitude) {
-			path.Dequeue();
-			transform.position = dest;
-		}
-
-		transform.Translate(step);
+	Vector3 computeAlignment() {
+		return new Vector3();
 	}
 
-	public void Select()
-	{
+	void Update() {
+	}
+
+	public void Select() {
 		material.shader = outlineShader;
 		material.SetColor("_OutlineColor", Color.red);
 	}
 
-	public void Unselect()
-	{
+	public void Unselect() {
 		material.shader = defaultShader;
 	}
 
-	public void MoveTo(Vector3 dest)
-	{
-		path.Clear();
-		path.Enqueue(dest);
+	public void MoveTo(Vector3 dest) {
+		agent.destination = dest;
 	}
 
-	public Vector3 Position { get {
-		return transform.position;
-	} }
+	public Vector3 Position {
+		get { return transform.position; }
+	}
 }
