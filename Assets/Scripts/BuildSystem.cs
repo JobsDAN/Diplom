8930078ffd208c1fe540fcd6f8356bf76c6b76d8
Diadlo <polsha3@gmail.com>
2017-09-22 01:04:19 +0300

    Add materials for [un]avaliable building

--- Assets/Scripts/BuildSystem.cs
+++ Assets/Scripts/BuildSystem.cs
@@ -9,7 +9,14 @@ public class BuildSystem : MonoBehaviour {
 	[SerializeField]
 	GameObject boxPrefab;
 
+	[SerializeField]
+	Material avaliableMaterial;
+
+	[SerializeField]
+	Material unavaliableMaterial;
+
 	GameObject currentObject;
+	Material currentMaterial;
 
 	Collider groundCollider;
 
@@ -21,39 +28,82 @@ public class BuildSystem : MonoBehaviour {
 	const int LEFT_MOUSE_BUTTON = 0;
 	const int RIGHT_MOUSE_BUTTON = 1;
 
+	bool BuildAvaliable()
+	{
+		Vector3 size = boxPrefab.GetComponent<Renderer>().bounds.size;
+		Vector3 pos = currentObject.transform.position;
+		Quaternion q = currentObject.transform.rotation;
+		foreach (Collider coll in Physics.OverlapBox(pos, size / 1.5f, q))
+		{
+			if (coll == currentObject.GetComponent<Collider>())
+				continue;
+
+			if (coll.gameObject.tag == "Building")
+				return false;
+		}
+
+		return true;
+	}
+
+	void CancelBuilding()
+	{
+		Destroy(currentObject);
+		currentObject = null;
+	}
+
+	void FollowMouse()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (!groundCollider.Raycast(ray, out hit, Mathf.Infinity))
+			return;
+
+		Renderer renderer = currentObject.GetComponent<Renderer>();
+		if (!renderer)
+			return;
+
+		Vector3 size = renderer.bounds.size;
+		Vector3 pos = hit.point;
+		pos.y = size.y / 2;
+		currentObject.transform.position = pos;
+	}
+
+	void ColorizeBuilding(bool avaliable)
+	{
+		Renderer renderer = currentObject.GetComponent<Renderer>();
+		renderer.material = avaliable ? avaliableMaterial : unavaliableMaterial;
+	}
+
+	void PlaceBuilding()
+	{
+		currentObject.GetComponent<NavMeshObstacle>().enabled = true;
+		currentObject.GetComponent<Renderer>().material = currentMaterial;
+		currentObject = null;
+	}
+
 	void Update () {
 		// Follow mouse
-		if (currentObject)
-		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (!groundCollider.Raycast(ray, out hit, Mathf.Infinity))
-			{
-				return;
-			}
-
-			Vector3 size = boxPrefab.GetComponent<Renderer>().bounds.size;
-			Vector3 pos = hit.point;
-			pos.y = size.y / 2;
-
-			currentObject.transform.position = pos;
-		}
+		if (!currentObject)
+			return;
 
 		// Right mouse click to cancel building
 		if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON))
 		{
-			Destroy(currentObject);
-			currentObject = null;
+			CancelBuilding();
 			return;
 		}
 
+		FollowMouse();
+		bool avaliable = BuildAvaliable();
+		ColorizeBuilding(avaliable);
+	
 		// Place on left mouse click
-		if (Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON) &&
+		if (avaliable &&
+			Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON) &&
 			// Avoid build under UI
 			!EventSystem.current.IsPointerOverGameObject())
 		{
-				currentObject.GetComponent<NavMeshObstacle>().enabled = true;
-				currentObject = null;
+			PlaceBuilding();
 		}
 	}
 
@@ -61,18 +111,16 @@ public class BuildSystem : MonoBehaviour {
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		if (!groundCollider.Raycast(ray, out hit, Mathf.Infinity))
-		{
 			return;
-		}
 
 		if (currentObject != null)
 		{
-			Destroy(currentObject);
-			currentObject = null;
+			CancelBuilding();
 			return;
 		}
 
 		Quaternion q = new Quaternion();
 		currentObject = Instantiate<GameObject>(boxPrefab, hit.point, q);
+		currentMaterial = currentObject.GetComponent<Renderer>().material;
 	}
 }
